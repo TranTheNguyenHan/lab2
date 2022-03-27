0d78271dac6e5b127006a6b949c010211835064a
unknown <ttnhan.20it5@vku.udn.vn>
2022-03-27 21:26:49 +0700

    first commit

---
+++ FindMyage/app/src/main/java/com/example/findmyage/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.findmyage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        buGetAge.setOnClickListener {
+            buttonClick()
+        }
+    }
+    fun buttonClick() {
+        val userDOB = Integer.parseInt(etDOB.text.toString())
+        val currentYear = Calendar.getInstance().get(Calendar.YEAR)
+        val userAgeInYears = currentYear - userDOB
+        tvShowAge.text = "Your Age is $userAgeInYears Years"
+    }
+}
